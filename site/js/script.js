d46a5e5533be7361c39ba6a6945c5652e460d309
manoe <manoekieffer@hotmail.fr>
2022-05-03 17:07:19 +0200

    now filters are collapsable, and checked boxes stay visible

--- site/js/script.js
+++ site/js/script.js
@@ -1,6 +1,38 @@
 
 /*Allows for 3 states checkboxes. Source: http://jsfiddle.net/chriscoyier/mGg85/2/ */
 function ts(cb) {
-    if (cb.readOnly) cb.checked = cb.readOnly = false;
-    else if (!cb.checked) cb.readOnly = cb.indeterminate = true;
+    if (cb.readOnly) {
+        cb.checked = cb.readOnly = false;
+    }
+    else if (!cb.checked) {
+        cb.readOnly = cb.indeterminate = true;
+    }
   }
+
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        var k;
+        var toCollapse = this.nextElementSibling;
+        console.log(this.nextElementSibling)
+        var lis = toCollapse.getElementsByTagName("li");
+        if (toCollapse.toHide === "true") {
+            toCollapse.toHide= "false";
+            
+            for(k = 0; k < lis.length; k++) {
+                /* if the checkbox is check or indeterminate, we let it be visible to the user */
+                if (!(lis[k].getElementsByTagName("input")[0].checked || lis[k].getElementsByTagName("input")[0].indeterminate)) {
+                    lis[k].hidden = "hidden";
+                }
+            }
+        } else {
+            toCollapse.toHide= "true";
+            for(k = 0; k < lis.length; k++) {
+                lis[k].hidden = "";
+            }
+    }
+  });
+}
